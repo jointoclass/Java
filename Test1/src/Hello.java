ce7c9879440b6f6969dd81291e5cd998894919e4
pepe <dltmdqls15@gmail.com>
2024-10-15 15:42:28 +0900

    Initial commit

---
+++ Test1/src/Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+    public static void main(String[] args) {
+        //파이션에 #과 같은 기능임으로 주석으로 사용 //오류시 괄호 체크해보기
+         System.out.println("Hello world");
+         System.out.println(1 + 2);
+         System.out.println("1" + "2");
+         System.out.println("3");
+         }
+    }
